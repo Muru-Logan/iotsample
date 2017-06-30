599a0f2943d3ab8a30b4fa0d43796ef6ed371b12
Murugesan Loganathan <Murugesan Loganathan>
2017-06-30 20:35:18 +0530

    first commit

---
+++ iothub-cloud/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.ServiceBus.Messaging;
+using System.Threading;
+using System.Text;
+using System.Collections.Generic;
+
+namespace iothub_cloud
+{
+    class Program
+    {
+        static string iotHubConnectionString = "HostName=muru-IoThub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=gkMVe8gqEqZlVsyaYujb4BvWyXDdyyIn53bVEFJVxZc=";
+        static string iotD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receiving messages, press Ctrl+C to quit");
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(iotHubConnectionString, iotD2cEndpoint);
+
+            var cancelTokenSource = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cancelTokenSource.Cancel();
+                Console.WriteLine("Quiting...");
+            };
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+            var tasks = new List<Task>();
+
+            foreach (var partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cancelTokenSource.Token));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+
+
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+
+            while (true)
+            {
+                if (ct.IsCancellationRequested) break;
+                var eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
+
+
+            }
+        }
+    }
+}
---
+++ iothub-sample/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+
+using Microsoft.Azure.Devices.Common.Exceptions;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace iothub_sample
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string iotHubConnectionString = "HostName=muru-IoThub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=gkMVe8gqEqZlVsyaYujb4BvWyXDdyyIn53bVEFJVxZc=";
+
+        static string iotHubUri = "muru-IoThub.azure-devices.net";
+        static string deviceId = "muru-device1";
+        static Device device;
+        static DeviceClient deviceClient;
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(iotHubConnectionString);
+            var deviceKey = AddDeviceAsync(deviceId).Result;
+
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey), Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+
+            Console.WriteLine("Client started sending messages...");
+
+            SendMessageToCloudAsync();
+
+            Console.ReadLine();
+
+        }
+
+        private static async Task<string> AddDeviceAsync(string deviceId)
+        {
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+  
+            Console.WriteLine("device key {0} = ", device.Authentication.SymmetricKey.PrimaryKey);
+
+            return device.Authentication.SymmetricKey.PrimaryKey;
+        }
+
+        private static async void SendMessageToCloudAsync()
+        {
+            double minTemperature = 20;
+            double minHumidity = 60;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = deviceId,
+                    temperature = currentTemperature,
+                    humidity = currentHumidity
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(messageString));
+                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(1000);
+            }
+
+        }
+    }
+}
